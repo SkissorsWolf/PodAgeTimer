acd6118a06427ae6819c27dd7a4e0a0c1ccd7b59
SkissorsWolf <adamsda1980@gmail.com>
2015-04-27 02:58:38 -0700

    - Removed the unused OnClickListener and Button references. - Added a handler to launch the main screen of the app after a brief delay on the welcome screen. - Added most of the interface for the main screen. - Changed typeface to sans. - Added and edited strings for UI elements.

--- app/src/main/java/net/skissorswolf/podagetimer/PodAgeTimerWelcome.java
+++ app/src/main/java/net/skissorswolf/podagetimer/PodAgeTimerWelcome.java
@@ -5,8 +5,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.os.Handler;
+
 
 
 public class PodAgeTimerWelcome extends Activity {
@@ -15,6 +15,14 @@ public class PodAgeTimerWelcome extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pod_age_timer_welcome);
+
+        int secondsDelayed = 6;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(PodAgeTimerWelcome.this, PodAgeTimerMain.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
     }
 
     public void launchBrowser(View view) {
